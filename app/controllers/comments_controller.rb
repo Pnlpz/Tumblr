65d25bf9cdd70d0b5ebc4295500feaaa3ee00200
Marisol Smith <marisolsmith@gmail.com>
2021-01-13 14:21:13 -0300

    Add destroy method and link to comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,14 @@ class CommentsController < ApplicationController
 		@comment = @post.comments.create(params[:comment].permit(:name, :body))
 
 		redirect_to post_path(@post)
-	end
+    end
+    
+    def destroy
+        @post = Post.find(params[:post_id])
+        @comment = @post.comments.find(params[:id])
+        @comment.destroy
+
+        redirect_to post_path(@post)
+    end
 
 end
